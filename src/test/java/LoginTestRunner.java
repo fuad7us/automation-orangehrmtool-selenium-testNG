c333253301e619c72417037c445381811fdb5d32
fuad7us <fuad7us@yahoo.com>
2022-10-26 11:09:43 +0600

    2 test case passed

--- src/test/java/LoginTestRunner.java
+++ src/test/java/LoginTestRunner.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTestRunner extends Setup{
@@ -6,5 +8,14 @@ public class LoginTestRunner extends Setup{
         LoginPage loginPage = new LoginPage(driver);
         driver.get("https://opensource-demo.orangehrmlive.com");
         loginPage.doLogin();
+        String urlActual = driver.getCurrentUrl();
+        String urlExpected = "viewEmployeeList";
+        Assert.assertTrue(urlActual.contains(urlExpected));
+    }
+
+    @Test
+    public void ifProfileImageExists(){
+        boolean imageDisplayStatus = driver.findElement(By.className("oxd-userdropdown-img")).isDisplayed();
+        Assert.assertTrue(imageDisplayStatus);
     }
 }
